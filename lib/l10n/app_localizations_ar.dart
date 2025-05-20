eae9929efcd616233da0804d2923ed7d6c7c3e1d
Hisham Aymen <heshamaymen8@gmail.com>
2025-05-21 02:45:34 +0300

    mac book installment

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,613 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forgetPassword => 'نسيت كلمة المرور؟';
+
+  @override
+  String get loginWithGoogle => 'تسجيل الدخول باستخدام Google';
+
+  @override
+  String get dontHaveAnAccountJoinUs => 'ليس لديك حساب؟ انضم الآن';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get home => 'جدول الحجوزات';
+
+  @override
+  String get myDates => 'مواعيدي';
+
+  @override
+  String get profile => 'الملف الشخصي';
+
+  @override
+  String get categories => 'الفئات';
+
+  @override
+  String get seeAll => 'عرض الكل';
+
+  @override
+  String get mostBookedDoctors => 'الأطباء الأقرب';
+
+  @override
+  String get heart => 'القلب';
+
+  @override
+  String get lung => 'الرئة';
+
+  @override
+  String get teeth => 'الأسنان';
+
+  @override
+  String get eye => 'العيون';
+
+  @override
+  String get general => 'عام';
+
+  @override
+  String get theInterior => 'الباطنية';
+
+  @override
+  String get nerves => 'الأعصاب';
+
+  @override
+  String get surgery => 'الجراحة';
+
+  @override
+  String get aboutDoctor => 'عن الطبيب';
+
+  @override
+  String get reviews => 'التقييمات';
+
+  @override
+  String get egp => 'جنيه مصري';
+
+  @override
+  String get reserveNow => 'احجز الآن';
+
+  @override
+  String get privacy => 'سياسة الخصوصية';
+
+  @override
+  String get callUs => 'اتصل بنا';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get myAccount => 'حسابي';
+
+  @override
+  String get noName => 'لا يوجد اسم متاح';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get noEmail => 'لا يوجد بريد إلكتروني متاح';
+
+  @override
+  String get phoneError => 'يرجى إدخال رقم هاتف صحيح: +20(XXX) XXX-XXXX';
+
+  @override
+  String get emptyPhone => 'يرجى إدخال رقم هاتفك';
+
+  @override
+  String get loginWithEmail => 'تسجيل الدخول باستخدام البريد الإلكتروني';
+
+  @override
+  String get phoneNumber => 'رقم الهاتف';
+
+  @override
+  String get pay => 'دفع';
+
+  @override
+  String get doctor => 'الطبيب';
+
+  @override
+  String get date => 'التاريخ';
+
+  @override
+  String get time => 'الوقت';
+
+  @override
+  String get review => 'مراجعة';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get choosePaymentMethod => 'اختر طريقة الدفع';
+
+  @override
+  String get electronicWallet => 'محفظة إلكترونية';
+
+  @override
+  String get bank => 'تحويل بنكي';
+
+  @override
+  String get meter => 'عداد';
+
+  @override
+  String get newWord => 'جديد';
+
+  @override
+  String get joinUs => 'انضم الآن';
+
+  @override
+  String get pending => 'قيد الانتظار';
+
+  @override
+  String get approved => 'مؤكد';
+
+  @override
+  String get cancelled => 'ملغي';
+
+  @override
+  String get completed => 'مكتمل';
+
+  @override
+  String get favourites => 'المفضلة';
+
+  @override
+  String get myReviews => 'مراجعتي';
+
+  @override
+  String get noPhoneNumberSet => 'لم يتم إدخال رقم الهاتف';
+
+  @override
+  String get medicalPrescription => 'روشتة طبية';
+
+  @override
+  String get deleteAccount => 'حذف الحساب';
+
+  @override
+  String get medicals => 'السجلات الطبية';
+
+  @override
+  String get medicalRumor => 'خرافة طبية';
+
+  @override
+  String get medicalAnalysis => 'تحليل طبي';
+
+  @override
+  String get noReviewsYet => 'لا توجد مراجعات حتى الآن';
+
+  @override
+  String get reserveDay => 'يوم الحجز';
+
+  @override
+  String get yourNumber => 'الرقم المحول منه';
+
+  @override
+  String get from => 'من';
+
+  @override
+  String get uploadScreenshot => 'صورة الايصال';
+
+  @override
+  String get selectNumber => 'اختر الرقم';
+
+  @override
+  String get reservationDetails => 'تفاصيل الحجز';
+
+  @override
+  String get writeReview => 'اكتب مراجعة';
+
+  @override
+  String get pleaseEnterReview => 'يرجى كتابة مراجعتك';
+
+  @override
+  String get addReview => 'إرسال المراجعة';
+
+  @override
+  String get reviewAddedSuccessfully => 'تم إرسال المراجعة بنجاح';
+
+  @override
+  String get isDoctorOnClinic => 'هل الطبيب متاح في العيادة؟';
+
+  @override
+  String get confirm => 'تأكيد';
+
+  @override
+  String get cancel => 'إلغاء';
+
+  @override
+  String get noDate => 'لم يتم اختيار تاريخ';
+
+  @override
+  String get noTimes => 'لا توجد مواعيد متاحة';
+
+  @override
+  String get noMedicalsYet => 'لا توجد سجلات طبية حتى الآن';
+
+  @override
+  String get noMedicalPrescriptionsYet => 'لا توجد روشتات طبية حتى الآن';
+
+  @override
+  String get noMedicalRumorsYet => 'لا توجد خرافات طبية حتى الآن';
+
+  @override
+  String get noMedicalAnalysisYet => 'لا توجد تحاليل طبية حتى الآن';
+
+  @override
+  String get noMedicals => 'لا توجد سجلات طبية';
+
+  @override
+  String get noMedicalPrescriptions => 'لا توجد روشتات طبية';
+
+  @override
+  String get noMedicalRumors => 'لا توجد خرافات طبية';
+
+  @override
+  String get noMedicalAnalysis => 'لا توجد تحاليل طبية';
+
+  @override
+  String get noFavouritesYet => 'لا توجد مفضلات حتى الآن';
+
+  @override
+  String get slot => 'الموعد';
+
+  @override
+  String get price => 'السعر';
+
+  @override
+  String get paymentMethod => 'طريقة الدفع';
+
+  @override
+  String get patientName => 'اسم المريض';
+
+  @override
+  String get patientPhoneNumber => 'رقم هاتف المريض';
+
+  @override
+  String get status => 'الحالة';
+
+  @override
+  String get yes => 'نعم';
+
+  @override
+  String get no => 'لا';
+
+  @override
+  String get reserveToYourSelf => 'احجز لنفسك';
+
+  @override
+  String get reserveToAnotherPatient => 'احجز لشخص آخر';
+
+  @override
+  String get takePhoto => 'التقاط صورة';
+
+  @override
+  String get chooseFromGallery => 'اختيار من المعرض';
+
+  @override
+  String get reservationPage => 'صفحة الحجز';
+
+  @override
+  String get totalAmount => 'القيمة الإجمالية';
+
+  @override
+  String get reservationCompletedWaitingToDoctorApproved => 'اكتمل الحجز - في انتظار موافقة الطبيب';
+
+  @override
+  String get patient => 'المريض';
+
+  @override
+  String get signUp => 'إنشاء حساب';
+
+  @override
+  String get haveAnAccountLogin => 'هل لديك حساب؟ تسجيل الدخول';
+
+  @override
+  String get description => 'الوصف';
+
+  @override
+  String get confirmPassword => 'تأكيد كلمة المرور';
+
+  @override
+  String get chooseYourSpecialist => 'اختر تخصصك';
+
+  @override
+  String get selectClinicLocation => 'اختر موقع العيادة';
+
+  @override
+  String get workingFrom => 'ساعات العمل تبدأ من';
+
+  @override
+  String get workingTo => 'ساعات العمل تنتهي عند';
+
+  @override
+  String get clinicInfo => 'معلومات العيادة';
+
+  @override
+  String get clinicWorkingFrom => 'يفتح العيادة من';
+
+  @override
+  String get clinicWorkingTo => 'يغلق العيادة عند';
+
+  @override
+  String get uploadCertificate => 'رفع الشهادة';
+
+  @override
+  String get fillProfile => 'إكمال الملف الشخصي';
+
+  @override
+  String get allDoctors => 'جميع الأطباء';
+
+  @override
+  String get citiesZones => 'المدن والأحياء';
+
+  @override
+  String get reserve => 'حجز موعد';
+
+  @override
+  String get to => 'إلى';
+
+  @override
+  String get money => 'المال';
+
+  @override
+  String get privacyAndPolicy => 'سياسة الخصوصية';
+
+  @override
+  String get withdraw => 'سحب';
+
+  @override
+  String get transactionsMoney => 'المعاملات المالية';
+
+  @override
+  String get day => 'اليوم';
+
+  @override
+  String get week => 'الأسبوع';
+
+  @override
+  String get year => 'السنة';
+
+  @override
+  String get today => 'اليوم';
+
+  @override
+  String get moneyRequest => 'طلب مالي';
+
+  @override
+  String get amount => 'المبلغ';
+
+  @override
+  String get yourPhoneNumber => 'رقم هاتفك';
+
+  @override
+  String get afterWithdrawAmount => 'المبلغ بعد السحب';
+
+  @override
+  String get request => 'طلب';
+
+  @override
+  String get anotherPhoneNumber => 'رقم هاتف آخر';
+
+  @override
+  String get resendCode => 'إعادة إرسال الرمز';
+
+  @override
+  String get moneyAdd => 'إضافة المال';
+
+  @override
+  String get obstetrics => 'التوليد';
+
+  @override
+  String get urology => 'المسالك البولية';
+
+  @override
+  String get pediatrics => 'طب الأطفال';
+
+  @override
+  String get psychiatry => 'الطب النفسي';
+
+  @override
+  String get ent => 'أنف وأذن وحنجرة';
+
+  @override
+  String get dermatology => 'الأمراض الجلدية';
+
+  @override
+  String get orthopedics => 'العظام';
+
+  @override
+  String get familyMedicineAndAllergy => 'الطب العام والحساسية';
+
+  @override
+  String get gastroenterology => 'أمراض الجهاز الهضمي';
+
+  @override
+  String get internalMedicine => 'الباطنة';
+
+  @override
+  String get acupuncture => 'الإبر الصينية';
+
+  @override
+  String get vascularSurgery => 'جراحة الأوعية الدموية';
+
+  @override
+  String get nephrology => 'أمراض الكلى';
+
+  @override
+  String get radiology => 'الأشعة';
+
+  @override
+  String get endocrinology => 'الغدد الصماء';
+
+  @override
+  String get genetics => 'الوراثة';
+
+  @override
+  String get speechTherapy => 'علاج النطق';
+
+  @override
+  String get painManagement => 'إدارة الألم';
+
+  @override
+  String get cosmeticSurgery => 'جراحة التجميل';
+
+  @override
+  String get nutritionist => 'أخصائي تغذية';
+
+  @override
+  String get revisionPage => 'مراجعه';
+
+  @override
+  String get cities => 'المدن';
+
+  @override
+  String get zones => 'الحي او المركز';
+
+  @override
+  String get application => 'Application';
+
+  @override
+  String get welcomeTo => 'مرحبا في';
+
+  @override
+  String get salamtk => 'سلامتك';
+
+  @override
+  String get signInUp => 'تسجيل دخول';
+
+  @override
+  String get signInNow => 'سجل دخول الان للبدء';
+
+  @override
+  String get contactUs => 'تواصل معنا';
+
+  @override
+  String get city => 'المحافظه';
+
+  @override
+  String get state => 'الحي او المركز';
+
+  @override
+  String get country => 'دوله';
+
+  @override
+  String get address => 'العنوان';
+
+  @override
+  String get area => 'الحي او المركز';
+
+  @override
+  String get street => 'الشارع';
+
+  @override
+  String get chooseFromCamera => 'التقط صوره';
+
+  @override
+  String get chooseOption => 'اختر';
+
+  @override
+  String get next => 'التالي';
+
+  @override
+  String get assistantDoctorNotifications => 'اشعارات الطبيب المساعد';
+
+  @override
+  String get addAssistantDoctor => 'اضافه طبيب مساعد';
+
+  @override
+  String get relatedDoctors => 'الأطباء المقترنون';
+
+  @override
+  String get noSupervisedDoctorsFound => 'لست ضمن لائحه اطباء';
+
+  @override
+  String get allRequests => 'جميع الطلبات';
+
+  @override
+  String get selectDoctor => 'اختر الطبيب';
+
+  @override
+  String get requestDoctor => 'طلب الطبيب';
+
+  @override
+  String get noDoctorDetailsFound => 'لايوجد بيانات اطباء';
+
+  @override
+  String get doctorNotifications => 'اشعارات الطبيب المساعد';
+
+  @override
+  String get explore => 'تصفح';
+
+  @override
+  String get specialist => 'التخصص';
+
+  @override
+  String get rating => 'التقييم';
+
+  @override
+  String get distinctiveMark => 'علامه مميزه';
+
+  @override
+  String get pleaseEnterStreet => 'من فضلك ادخل الشارع';
+
+  @override
+  String get userHome => 'الرئيسيه';
+
+  @override
+  String get doctorAddedSuccefully => 'تم اضافه الطبيب بنجاح';
+
+  @override
+  String get pleaseSelectWorkingTo => 'من فضلك اختر وقت الانتهاء';
+
+  @override
+  String get pleaseCheckClinicInfo => 'من فضلك ادخل بيانات العياده';
+
+  @override
+  String get pleaseSelectWorkingFrom => 'من فضلك اختر وقت البدء';
+
+  @override
+  String get pleaseUploadYourImage => 'من فضلك قم بتحميل صورتك';
+
+  @override
+  String get pleaseUploadYourCertificate => 'من فضلك قم بتحميل شهادتك';
+
+  @override
+  String get pleaseEnterPrice => 'من فضلك ادخل السعر';
+
+  @override
+  String get somethingWentWrong => 'حدث خطأ ما';
+
+  @override
+  String get pleasEnterYourPassword => 'من فضلك ادخل كلمة المرور';
+
+  @override
+  String get emailOrPasswordIncorrect => 'البريد الالكتروني او كلمة المرور غير صحيحه';
+
+  @override
+  String get error => 'خطأ';
+
+  @override
+  String get welcomeBack => 'مرحبا بعودتك';
+
+  @override
+  String get workingTimes => 'وقت العمل';
+
+  @override
+  String get workingDays => 'ايام العمل';
+
+  @override
+  String get clinicLocation => 'عنوان العياده';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,613 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgetPassword => 'Forgot Password?';
+
+  @override
+  String get loginWithGoogle => 'Sign in with Google';
+
+  @override
+  String get dontHaveAnAccountJoinUs => 'Don\'t have an account? Join now';
+
+  @override
+  String get login => 'Log In';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get myDates => 'My Appointments';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get categories => 'Categories';
+
+  @override
+  String get seeAll => 'See All';
+
+  @override
+  String get mostBookedDoctors => 'Nearest Doctors';
+
+  @override
+  String get heart => 'Heart';
+
+  @override
+  String get lung => 'Lung';
+
+  @override
+  String get teeth => 'Teeth';
+
+  @override
+  String get eye => 'Eye';
+
+  @override
+  String get general => 'General';
+
+  @override
+  String get theInterior => 'interior';
+
+  @override
+  String get nerves => 'Nerves';
+
+  @override
+  String get surgery => 'Surgery';
+
+  @override
+  String get aboutDoctor => 'About the Doctor';
+
+  @override
+  String get reviews => 'Reviews';
+
+  @override
+  String get egp => 'EGP';
+
+  @override
+  String get reserveNow => 'Book Now';
+
+  @override
+  String get privacy => 'Privacy Policy';
+
+  @override
+  String get callUs => 'Contact Us';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get myAccount => 'My Account';
+
+  @override
+  String get noName => 'No Name Available';
+
+  @override
+  String get logout => 'Sign Out';
+
+  @override
+  String get noEmail => 'No Email Available';
+
+  @override
+  String get phoneError => 'Please enter a valid phone number: +20(XXX) XXX-XXXX';
+
+  @override
+  String get emptyPhone => 'Please provide your phone number';
+
+  @override
+  String get loginWithEmail => 'Sign in with Email';
+
+  @override
+  String get phoneNumber => 'Phone Number';
+
+  @override
+  String get pay => 'Pay';
+
+  @override
+  String get doctor => 'Doctor';
+
+  @override
+  String get date => 'Date';
+
+  @override
+  String get time => 'Time';
+
+  @override
+  String get review => 'Review';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get choosePaymentMethod => 'Select Payment Method';
+
+  @override
+  String get electronicWallet => 'Digital Wallet';
+
+  @override
+  String get bank => 'Bank Transfer';
+
+  @override
+  String get meter => 'Meter';
+
+  @override
+  String get newWord => 'New';
+
+  @override
+  String get joinUs => 'Join Now';
+
+  @override
+  String get pending => 'Pending';
+
+  @override
+  String get approved => 'Approved';
+
+  @override
+  String get cancelled => 'Cancelled';
+
+  @override
+  String get completed => 'Completed';
+
+  @override
+  String get favourites => 'Favorites';
+
+  @override
+  String get myReviews => 'My Reviews';
+
+  @override
+  String get noPhoneNumberSet => 'Phone Number Not Set';
+
+  @override
+  String get medicalPrescription => 'Medical Prescription';
+
+  @override
+  String get deleteAccount => 'Delete Account';
+
+  @override
+  String get medicals => 'Medical Records';
+
+  @override
+  String get medicalRumor => 'Medical Myth';
+
+  @override
+  String get medicalAnalysis => 'Medical Analysis';
+
+  @override
+  String get noReviewsYet => 'No Reviews Yet';
+
+  @override
+  String get reserveDay => 'Appointment Day';
+
+  @override
+  String get yourNumber => 'Your Contact Number';
+
+  @override
+  String get from => 'From';
+
+  @override
+  String get uploadScreenshot => 'Upload Screenshot';
+
+  @override
+  String get selectNumber => 'Choose Contact Number';
+
+  @override
+  String get reservationDetails => 'Appointment Details';
+
+  @override
+  String get writeReview => 'Write a Review';
+
+  @override
+  String get pleaseEnterReview => 'Please type your review';
+
+  @override
+  String get addReview => 'Submit Review';
+
+  @override
+  String get reviewAddedSuccessfully => 'Review submitted successfully';
+
+  @override
+  String get isDoctorOnClinic => 'Is Doctor On Clinic';
+
+  @override
+  String get confirm => 'Confirm';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get noDate => 'No Date Selected';
+
+  @override
+  String get noTimes => 'No Available Times';
+
+  @override
+  String get noMedicalsYet => 'No Medical Records Yet';
+
+  @override
+  String get noMedicalPrescriptionsYet => 'No Prescriptions Yet';
+
+  @override
+  String get noMedicalRumorsYet => 'No Medical Myths Yet';
+
+  @override
+  String get noMedicalAnalysisYet => 'No Analyses Yet';
+
+  @override
+  String get noMedicals => 'No Medical Records';
+
+  @override
+  String get noMedicalPrescriptions => 'No Prescriptions';
+
+  @override
+  String get noMedicalRumors => 'No Medical Myths';
+
+  @override
+  String get noMedicalAnalysis => 'No Medical Analyses';
+
+  @override
+  String get noFavouritesYet => 'No Favorites Yet';
+
+  @override
+  String get slot => 'Slot';
+
+  @override
+  String get price => 'Price';
+
+  @override
+  String get paymentMethod => 'Payment Method';
+
+  @override
+  String get patientName => 'Patient Name';
+
+  @override
+  String get patientPhoneNumber => 'Patient Phone Number';
+
+  @override
+  String get status => 'Status';
+
+  @override
+  String get yes => 'Yes';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get reserveToYourSelf => 'Book for Yourself';
+
+  @override
+  String get reserveToAnotherPatient => 'Book for Another Patient';
+
+  @override
+  String get takePhoto => 'Take a Photo';
+
+  @override
+  String get chooseFromGallery => 'Choose from Gallery';
+
+  @override
+  String get reservationPage => 'Booking Page';
+
+  @override
+  String get totalAmount => 'Total Amount';
+
+  @override
+  String get reservationCompletedWaitingToDoctorApproved => 'Booking Completed - Awaiting Doctor\'s Approval';
+
+  @override
+  String get patient => 'Patient';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get haveAnAccountLogin => 'Already have an account? Log In';
+
+  @override
+  String get description => 'Description';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get chooseYourSpecialist => 'Choose Your Specialist';
+
+  @override
+  String get selectClinicLocation => 'Select Clinic Location';
+
+  @override
+  String get workingFrom => 'Working Hours Start';
+
+  @override
+  String get workingTo => 'Working Hours End';
+
+  @override
+  String get clinicInfo => 'Clinic Information';
+
+  @override
+  String get clinicWorkingFrom => 'Clinic Opens At';
+
+  @override
+  String get clinicWorkingTo => 'Clinic Closes At';
+
+  @override
+  String get uploadCertificate => 'Upload Certificate';
+
+  @override
+  String get fillProfile => 'Complete Profile';
+
+  @override
+  String get allDoctors => 'All Doctors';
+
+  @override
+  String get citiesZones => 'Cities & Zones';
+
+  @override
+  String get reserve => 'Book Appointment';
+
+  @override
+  String get to => 'To';
+
+  @override
+  String get money => 'Money';
+
+  @override
+  String get privacyAndPolicy => 'Privacy Policy';
+
+  @override
+  String get withdraw => 'Withdraw';
+
+  @override
+  String get transactionsMoney => 'Money Transactions';
+
+  @override
+  String get day => 'Day';
+
+  @override
+  String get week => 'Week';
+
+  @override
+  String get year => 'Year';
+
+  @override
+  String get today => 'Today';
+
+  @override
+  String get moneyRequest => 'Money Request';
+
+  @override
+  String get amount => 'Amount';
+
+  @override
+  String get yourPhoneNumber => 'Your Phone Number';
+
+  @override
+  String get afterWithdrawAmount => 'Amount After Withdrawal';
+
+  @override
+  String get request => 'Request';
+
+  @override
+  String get anotherPhoneNumber => 'Another Phone Number';
+
+  @override
+  String get resendCode => 'Resend Code';
+
+  @override
+  String get moneyAdd => 'Add Money';
+
+  @override
+  String get obstetrics => 'Obstetrics';
+
+  @override
+  String get urology => 'Urology';
+
+  @override
+  String get pediatrics => 'Pediatrics';
+
+  @override
+  String get psychiatry => 'Psychiatry';
+
+  @override
+  String get ent => 'Ear, Nose & Throat (ENT)';
+
+  @override
+  String get dermatology => 'Dermatology';
+
+  @override
+  String get orthopedics => 'Orthopedics';
+
+  @override
+  String get familyMedicineAndAllergy => 'Family Medicine & Allergy';
+
+  @override
+  String get gastroenterology => 'Gastroenterology';
+
+  @override
+  String get internalMedicine => 'Internal Medicine';
+
+  @override
+  String get acupuncture => 'Acupuncture';
+
+  @override
+  String get vascularSurgery => 'Vascular Surgery';
+
+  @override
+  String get nephrology => 'Nephrology';
+
+  @override
+  String get radiology => 'Radiology';
+
+  @override
+  String get endocrinology => 'Endocrinology';
+
+  @override
+  String get genetics => 'Genetics';
+
+  @override
+  String get speechTherapy => 'Speech Therapy';
+
+  @override
+  String get painManagement => 'Pain Management';
+
+  @override
+  String get cosmeticSurgery => 'Cosmetic Surgery';
+
+  @override
+  String get nutritionist => 'Nutritionist';
+
+  @override
+  String get revisionPage => 'Revision Page';
+
+  @override
+  String get cities => 'Cities';
+
+  @override
+  String get zones => 'Zones';
+
+  @override
+  String get application => 'Application';
+
+  @override
+  String get welcomeTo => 'Welcome To';
+
+  @override
+  String get salamtk => 'Salamutk';
+
+  @override
+  String get signInUp => 'Sign In/Up';
+
+  @override
+  String get signInNow => 'Sign In Now To Get Started';
+
+  @override
+  String get contactUs => 'Contact Us';
+
+  @override
+  String get city => 'City';
+
+  @override
+  String get state => 'State';
+
+  @override
+  String get country => 'Country';
+
+  @override
+  String get address => 'Address';
+
+  @override
+  String get area => 'Area';
+
+  @override
+  String get street => 'Street';
+
+  @override
+  String get chooseFromCamera => 'Choose From Camera';
+
+  @override
+  String get chooseOption => 'Choose Option';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get assistantDoctorNotifications => 'Assistant Doctor Notifications';
+
+  @override
+  String get addAssistantDoctor => 'Add Assistant Doctor';
+
+  @override
+  String get relatedDoctors => 'Related Doctors';
+
+  @override
+  String get noSupervisedDoctorsFound => 'No Supervised Doctors Found';
+
+  @override
+  String get allRequests => 'All Requests';
+
+  @override
+  String get selectDoctor => 'Select Doctor';
+
+  @override
+  String get requestDoctor => 'Request Doctor';
+
+  @override
+  String get noDoctorDetailsFound => 'No Doctor Details Found';
+
+  @override
+  String get doctorNotifications => 'Doctor Notifications';
+
+  @override
+  String get explore => 'Explore';
+
+  @override
+  String get specialist => 'Specialist';
+
+  @override
+  String get rating => 'Rating';
+
+  @override
+  String get distinctiveMark => 'Distinctive Mark';
+
+  @override
+  String get pleaseEnterStreet => 'Please Enter Street';
+
+  @override
+  String get userHome => 'Home';
+
+  @override
+  String get doctorAddedSuccefully => 'Doctor Added Successfully';
+
+  @override
+  String get pleaseSelectWorkingTo => 'Please Select Working To';
+
+  @override
+  String get pleaseCheckClinicInfo => 'Please Check Clinic Info';
+
+  @override
+  String get pleaseSelectWorkingFrom => 'Please Select Working From';
+
+  @override
+  String get pleaseUploadYourImage => 'Please Upload Your Image';
+
+  @override
+  String get pleaseUploadYourCertificate => 'Please Upload Your Certificate';
+
+  @override
+  String get pleaseEnterPrice => 'Please Enter Price';
+
+  @override
+  String get somethingWentWrong => 'Something Went Wrong';
+
+  @override
+  String get pleasEnterYourPassword => 'Please Enter Your Password';
+
+  @override
+  String get emailOrPasswordIncorrect => 'Email Or Password Incorrect';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get welcomeBack => 'Welcome Back';
+
+  @override
+  String get workingTimes => 'Working Times';
+
+  @override
+  String get workingDays => 'Working Days';
+
+  @override
+  String get clinicLocation => 'Clinic Location';
+}
